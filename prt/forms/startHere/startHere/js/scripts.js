b50946b27c28adab0597dd46b6276a4579483310
maguilar08 <Marcelo_AguilarSBS@outlook.com>
2026-02-14 09:28:05 -0400

    Forms completed

---
+++ prt/forms/startHere/startHere/js/scripts.js
@@ -0,0 +1,25 @@
+//const getString = window.location.search;
+//console.log(getString);
+
+//const myInfo = new URLSearchParams(getString)
+//console.log(myInfo);
+
+//The same just with this
+const myInfo = new URLSearchParams(window.location.search)
+console.log(myInfo);
+//--------------------------------------------------------
+
+//Just to display in the console
+//console.log(myInfo.get('first'));
+//console.log(myInfo.get('last'));
+//console.log(myInfo.get('ordinance'));
+//console.log(myInfo.get('date'));
+//console.log(myInfo.get('location'));
+//console.log(myInfo.get('phone'));
+//console.log(myInfo.get('email'));
+
+document.querySelector('#results').innerHTML =`
+<P>Appointment for ${myInfo.get('first')} ${myInfo.get('last')}</p>
+<p>Proxy ${myInfo.get('ordinance')} on ${myInfo.get('date')} in the ${myInfo.get('location')} Temple</p>
+<p>Your Phone: ${myInfo.get('phone')}</p>
+<p>Your Email is: ${myInfo.get('email')}</p>`
